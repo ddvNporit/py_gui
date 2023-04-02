c64e898f92893b1f06c1e5dd367bb1fae14f3767
ddv5 <dima.nporit@gmail.com>
2023-04-02 09:24:10 +0300

    generation data

--- generator/groups.py
+++ generator/groups.py
@@ -1,14 +1,52 @@
 # Name : groups.py
 # Author : "Denisov Dmitry"
 # Time : 01.04.2023
-import os
+import os, random, string
 from comtypes.client import CreateObject
 
+n = 3
 project_dir = os.path.dirname(os.path.dirname(os.path.realpath(__file__)))
-xl = CreateObject("Excel.Application")
-xl.Visible = 1
-wb = xl.Workbooks.Add()
-for i in range(10):
-    xl.Range["A%s" % (i+1)].Value[()] = "group %s" %i
-wb.SaveAs(os.path.join(project_dir, "groups.xlsx"))
-xl.Quit()
+file = os.path.join(project_dir, "groups.xlsx")
+
+
+def random_string(prefix, maxlen):
+    # symbols = string.ascii_letters + string.digits + string.punctuation + " " * 10
+    symbols = string.ascii_letters + string.digits + " " * 10
+    return prefix + "".join([random.choice(symbols) for i in range(random.randrange(maxlen))])
+
+
+def generator_data():
+    global testdata
+    testdata = [
+        random_string("name", 10)
+        for i in range(n)
+    ]
+
+
+def greate_file_xlsx():
+    xl = CreateObject("Excel.Application")
+    xl.Visible = 0
+    wb = xl.Workbooks.Add()
+    i = 0
+    generator_data()
+    while i < len(testdata):
+        xl.Range["A%s" % (i + 1)].Value[()] = testdata[i]
+        i += 1
+    if os.path.isfile(file):
+        os.remove(file)
+    wb.SaveAs(file)
+    xl.Quit()
+
+
+greate_file_xlsx()
+
+def read_data_xlsx():
+    xl = CreateObject("Excel.Application")
+    xl.Visible = 0
+    wb = xl.Workbooks.Open(file)
+    sheet = wb.ActiveSheet
+    val = sheet.Cells(1, 1).value
+    print(val)
+    print(repr(val))
+    xl.Quit()
+read_data_xlsx()
--- tests/test_add_group.py
+++ tests/test_add_group.py
@@ -1,6 +1,12 @@
 # Name : test_add_group.py
 # Author : "Denisov Dmitry"
 # Time : 31.03.2023
+from comtypes.client import CreateObject
+def read_data_xlsx():
+    xl = CreateObject("Excel.Application")
+    xl.Visible = 1
+    wb = xl.Workbooks("groups.xlsx").Activate
+read_data_xlsx()
 def test_add_group(app):
     old_list = app.groups.get_group_list()
     app.groups.add_new_group("my group")
