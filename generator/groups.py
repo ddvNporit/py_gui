116c3c7a0d323075bb2a17d47c9aa348dd2bf35b
ddv5 <dima.nporit@gmail.com>
2023-04-03 18:22:56 +0300

    generation data

--- generator/groups.py
+++ generator/groups.py
@@ -4,49 +4,52 @@
 import os, random, string
 from comtypes.client import CreateObject
 
-n = 3
-project_dir = os.path.dirname(os.path.dirname(os.path.realpath(__file__)))
-file = os.path.join(project_dir, "groups.xlsx")
 
+class GenerateData():
+    n = 3
+    project_dir = os.path.dirname(os.path.dirname(os.path.realpath(__file__)))
+    file = os.path.join(project_dir, "groups.xlsx")
 
-def random_string(prefix, maxlen):
-    # symbols = string.ascii_letters + string.digits + string.punctuation + " " * 10
-    symbols = string.ascii_letters + string.digits + " " * 10
-    return prefix + "".join([random.choice(symbols) for i in range(random.randrange(maxlen))])
+    def random_string(self, prefix, maxlen):
+        # symbols = string.ascii_letters + string.digits + string.punctuation + " " * 10
+        symbols = string.ascii_letters + string.digits + " " * 10
+        return prefix + "".join([random.choice(symbols) for i in range(random.randrange(maxlen))])
 
+    def generator_data(self, n):
+        global testdata
+        testdata = [
+            self.random_string("name", 10)
+            for i in range(n)
+        ]
 
-def generator_data():
-    global testdata
-    testdata = [
-        random_string("name", 10)
-        for i in range(n)
-    ]
+    def greate_file_xlsx(self, file):
+        xl = CreateObject("Excel.Application")
+        xl.Visible = 0
+        wb = xl.Workbooks.Add()
+        i = 0
+        self.generator_data()
+        while i < len(testdata):
+            xl.Range["A%s" % (i + 1)].Value[()] = testdata[i]
+            i += 1
+        if os.path.isfile(file):
+            os.remove(file)
+        wb.SaveAs(file)
+        xl.Quit()
 
-
-def greate_file_xlsx():
-    xl = CreateObject("Excel.Application")
-    xl.Visible = 0
-    wb = xl.Workbooks.Add()
-    i = 0
-    generator_data()
-    while i < len(testdata):
-        xl.Range["A%s" % (i + 1)].Value[()] = testdata[i]
-        i += 1
-    if os.path.isfile(file):
-        os.remove(file)
-    wb.SaveAs(file)
-    xl.Quit()
-
-
-greate_file_xlsx()
-
-def read_data_xlsx():
-    xl = CreateObject("Excel.Application")
-    xl.Visible = 0
-    wb = xl.Workbooks.Open(file)
-    sheet = wb.ActiveSheet
-    val = sheet.Cells(1, 1).value
-    print(val)
-    print(repr(val))
-    xl.Quit()
-read_data_xlsx()
+    def read_data_xlsx(self, file):
+        xl = CreateObject("Excel.Application")
+        xl.Visible = 0
+        # project_dir = os.path.dirname(os.path.dirname(os.path.realpath(__file__)))
+        # file = os.path.join(project_dir, "groups.xlsx")
+        wb = xl.Workbooks.Open(file)
+        sheet = wb.ActiveSheet
+        i = 1
+        list_groups = []
+        while sheet.Cells(i, 1).value() is not None:
+            val = sheet.Cells(i, 1).value()
+            list_groups.append(val)
+            i += 1
+            if i >= 100:
+                break
+        xl.Quit()
+        return list_groups
--- tests/test_add_group.py
+++ tests/test_add_group.py
@@ -2,14 +2,20 @@
 # Author : "Denisov Dmitry"
 # Time : 31.03.2023
 from comtypes.client import CreateObject
-def read_data_xlsx():
-    xl = CreateObject("Excel.Application")
-    xl.Visible = 1
-    wb = xl.Workbooks("groups.xlsx").Activate
-read_data_xlsx()
+from generator.groups import GenerateData
+import os
+
+
 def test_add_group(app):
+    data = GenerateData()
+    project_dir = os.path.dirname(os.path.dirname(os.path.realpath(__file__)))
+    file = os.path.join(project_dir, "groups.xlsx")
     old_list = app.groups.get_group_list()
-    app.groups.add_new_group("my group")
-    new_list = app.groups.get_group_list()
-    old_list.append("my group")
-    assert sorted(old_list) == sorted(new_list)
+    list_groups = data.read_data_xlsx(file)
+    i = 0
+    while i < len(list_groups):
+        app.groups.add_new_group(list_groups[i])
+        old_list.append(list_groups[i])
+        new_list = app.groups.get_group_list()
+        i += 1
+        assert sorted(old_list) == sorted(new_list)
